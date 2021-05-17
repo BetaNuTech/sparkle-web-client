23291e5aad1ab8d8b550d37d9cea0d918baced4a
Matthew Jensen <mjens02@gmail.com>
2021-05-17 10:49:06 -0500

    #15 add skeleton style system from web portal

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,5 @@
+import '../styles/app.scss';
+
 import React from 'react';
 import { ProvideAuth } from '../navigation/Auth/AuthProvider';
 import { PrivateRoute } from '../navigation/Auth/PrivateRoute';
