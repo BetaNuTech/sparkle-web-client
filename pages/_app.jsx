bd93b04a028b7e85dfb5145756095002c31a92d6
Matthew Jensen <mjens02@gmail.com>
2021-05-21 15:20:50 -0500

    #38 add pwa support & add #37 app icons

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,18 @@
 import '../styles/app.scss';
-
 import React from 'react';
 import { ProvideAuth } from '../navigation/Auth/AuthProvider';
 import { PrivateRoute } from '../navigation/Auth/PrivateRoute';
+import { NextHeader } from '../components/common/NextHeader';
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<ProvideAuth>
-			<PrivateRoute>
-				<Component {...pageProps} />
-			</PrivateRoute>
-		</ProvideAuth>
-	);
+  return (
+    <ProvideAuth>
+      <PrivateRoute>
+        <NextHeader />
+        <Component {...pageProps} />
+      </PrivateRoute>
+    </ProvideAuth>
+  );
 }
 
 export default MyApp;
