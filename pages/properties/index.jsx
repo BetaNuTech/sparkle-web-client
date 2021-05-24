cff765dd32e15f9e7e60d7a88fff23cf72425c20
Matthew Jensen <mjens02@gmail.com>
2021-05-24 15:02:10 -0500

    #41 add offline state UI

---
+++ pages/properties/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DefaultLayout } from '../../components/layouts/default';
+
+export default function Properties() {
+  return (
+    <DefaultLayout>
+      <div>Properties</div>
+    </DefaultLayout>
+  );
+}
