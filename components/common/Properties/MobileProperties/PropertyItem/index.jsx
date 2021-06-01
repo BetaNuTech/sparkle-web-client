090771cc3907f2ec0ee53a73534b825c439b97a3
Matthew Jensen <mjens02@gmail.com>
2021-06-01 16:22:02 -0500

    #55 add mobile swipe buttons and fix manifest.json

--- components/common/Properties/MobileProperties/PropertyItem/index.jsx
+++ components/common/Properties/MobileProperties/PropertyItem/index.jsx
@@ -1,8 +1,12 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { useState, useRef } from 'react';
 import styles from './PropertyItem.module.scss';
 import { TeamValues } from '../../../../shared/TeamValues';
+import { SwipeReveal } from '../SwipeReveal';
+import useSwipeReveal from '../../../../../hooks/useSwipeReveal';
+import ChevronIcon from '../../../../../public/icons/ios/chevron.svg';
 
 export const PropertyItem = ({
   backgroundImage,
@@ -10,58 +14,82 @@ export const PropertyItem = ({
   addr1,
   mailingAddr2,
   lastInspectionEntries
-}) => (
-  <div className={styles.propertyItem}>
-    {/* Toggle Button */}
-    <div className={styles.propertyItem__toggle} data-action="toggle-reveal" />
+}) => {
+  const [isSwipeOpen, setIsSwipeOpen] = useState(false);
+  const ref = useRef(null);
+  useSwipeReveal(ref, setIsSwipeOpen);
 
-    <Link href="/property">
-      <a className={styles.propertyItem__wrapper}>
-        {/* Profile Picture */}
-        <aside
-          className={styles.propertyItem__image}
-          style={
-            backgroundImage && { backgroundImage: `url('${backgroundImage}')` }
-          }
-        />
+  return (
+    <div ref={ref} className={styles.propertyItem}>
+      {/* Main Content */}
+      <div
+        className={
+          isSwipeOpen
+            ? clsx(
+                styles.propertyItem__content,
+                styles['propertyItem--swipeOpen']
+              )
+            : styles.propertyItem__content
+        }
+      >
+        {/* Toggle Button */}
+        <a className={styles.propertyItem__toggle}>
+          <ChevronIcon />
+        </a>
 
-        {/* Profile Description */}
-        <div className={styles.propertyItem__description}>
-          <h6 className={styles.propertyItem__name}>{name}</h6>
-          <p className={styles.propertyItem__addr1}>{addr1}</p>
-          <p className={styles.propertyItem__mailingAddr2}>{mailingAddr2}</p>
-          {lastInspectionEntries ? (
-            <p className={styles.propertyItem__lastInspectionEntries}>
-              {lastInspectionEntries}
-            </p>
-          ) : (
-            <p
-              className={clsx(
-                styles.propertyItem__lastInspectionEntries,
-                styles['propertyItem__lastInspectionEntries--empty']
+        <Link href="/property">
+          <a className={styles.propertyItem__wrapper}>
+            {/* Profile Picture */}
+            <aside
+              className={styles.propertyItem__image}
+              style={
+                backgroundImage && {
+                  backgroundImage: `url('${backgroundImage}')`
+                }
+              }
+            />
+
+            {/* Profile Description */}
+            <div className={styles.propertyItem__description}>
+              <h6 className={styles.propertyItem__name}>{name}</h6>
+              <p className={styles.propertyItem__addr1}>{addr1}</p>
+              <p className={styles.propertyItem__mailingAddr2}>
+                {mailingAddr2}
+              </p>
+              {lastInspectionEntries ? (
+                <p className={styles.propertyItem__lastInspectionEntries}>
+                  {lastInspectionEntries}
+                </p>
+              ) : (
+                <p
+                  className={clsx(
+                    styles.propertyItem__lastInspectionEntries,
+                    styles['propertyItem__lastInspectionEntries--empty']
+                  )}
+                >
+                  No Inspections
+                </p>
               )}
-            >
-              No Inspections
-            </p>
-          )}
-        </div>
-      </a>
-    </Link>
+            </div>
+          </a>
+        </Link>
 
-    {/* Metadata */}
-    <Link href="/lalala">
-      <a className={styles.propertyItem__metadata}>
-        Deficient Items
-        <TeamValues
-          numOfDeficientItems={2}
-          numOfFollowUpActionsForDeficientItems={2}
-          numOfRequiredActionsForDeficientItems={4}
-          isNarrowField={false}
-        />
-      </a>
-    </Link>
-  </div>
-);
+        {/* Metadata */}
+        <div className={styles.propertyItem__metadata}>
+          Deficient Items
+          <TeamValues
+            numOfDeficientItems={2}
+            numOfFollowUpActionsForDeficientItems={2}
+            numOfRequiredActionsForDeficientItems={4}
+            isNarrowField={false}
+          />
+        </div>
+      </div>
+
+      <SwipeReveal />
+    </div>
+  );
+};
 
 PropertyItem.propTypes = {
   backgroundImage: PropTypes.string,
