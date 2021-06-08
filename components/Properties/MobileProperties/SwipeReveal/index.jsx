2b1e5a2becac1ec501990d59d07f6968808e827a
Matthew Jensen <mjens02@gmail.com>
2021-06-08 11:30:21 -0500

    #62 remove unusued directories

--- components/Properties/MobileProperties/SwipeReveal/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './SwipeReveal.module.scss';
-
-export const SwipeReveal = () => (
-  <div className={styles.swipeReveal}>
-    <button className={styles.swipeReveal__editButton}>Edit</button>
-
-    <button className={styles.swipeReveal__deleteButton}>Delete</button>
-  </div>
-);
--- redux/ducks/properties/selectors.js
+++
@@ -1,7 +0,0 @@
-export const selectStateOfProperties = (state) => state.properties;
-
-export const selectActiveSortOfProperties = (state) =>
-  selectStateOfProperties(state).activeSort;
-
-export const selectItemsOfProperties = (state) =>
-  selectStateOfProperties(state).items;
