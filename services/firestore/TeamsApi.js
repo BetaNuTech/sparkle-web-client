b4a35a02e6af72b4e9f573bc4824f44646fda700
Matthew Jensen <mjens02@gmail.com>
2021-06-12 20:43:29 -0400

    #61 remove duplicate services directory

--- services/firestore/TeamsApi.js
+++
@@ -1,9 +0,0 @@
-import teamsMock from '../../__mocks__/PropertiesPage/teamsMock.json';
-
-export const TeamsApi = {
-  async fetchDataOfTeams() {
-    const data = teamsMock;
-
-    return data;
-  }
-};
