a9863d9122e661202c3dbb5d58811bd1574bda76
Matthew Jensen <mjens02@gmail.com>
2021-07-13 16:40:29 -0500

    #84 add lazy loaded GA tracking snippet in production

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,31 @@
+import { useEffect } from 'react';
 import { FirebaseAppProvider } from 'reactfire';
 import { ToastProvider } from 'react-toast-notifications';
+import { useRouter } from 'next/router';
 import AuthProvider from '../common/Auth/Provider';
 import PrivateRoute from '../common/Auth/PrivateRoute';
 import { NextHeader } from '../common/NextHeader';
 import errorReports from '../common/services/api/errorReports';
+import * as gtag from '../common/utils/gtag';
 import firebaseConfig from '../config/firebase';
 import '../styles/app.scss';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <ToastProvider>
---
+++ pages/_document.tsx
@@ -0,0 +1,54 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document';
+import Script from 'next/script';
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+class MyDocument extends Document {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          {IS_PRODUCTION && GA_TRACKING_ID && (
+            <>
+              <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                strategy="lazyOnload"
+              />
+              <Script strategy="lazyOnload">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `}
+              </Script>
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
