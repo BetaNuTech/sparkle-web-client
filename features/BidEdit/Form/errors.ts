73eb0664cdce114ba1dd4da43f1e5d9d99fa1a55
Matthew Jensen <mjens02@gmail.com>
2021-08-13 18:14:56 -0500

    #213 Edit job bid form and create new bid link dropdown

---
+++ features/BidEdit/Form/errors.ts
@@ -0,0 +1,3 @@
+export default {
+  vendorRequired: 'Please fill in vendor name.'
+};
--- features/JobBids/MobileLayout/index.tsx
+++ features/JobBids/MobileLayout/index.tsx
@@ -1,8 +1,12 @@
 import { FunctionComponent } from 'react';
+import clsx from 'clsx';
+import getConfig from 'next/config';
+import Link from 'next/link';
 import propertyModel from '../../../common/models/property';
 import jobModel from '../../../common/models/job';
 import bidModel from '../../../common/models/bid';
 import MobileHeader from '../../../common/MobileHeader';
+import AddIcon from '../../../public/icons/ios/add.svg';
 import BidSections from './BidSections';
 import styles from './styles.module.scss';
 
@@ -28,26 +32,45 @@ const MobileLayout: FunctionComponent<Props> = ({
   propertyId,
   colors,
   configBids
-}) => (
-  <>
-    <MobileHeader
-      title={`Bids (${bids.length})`}
-      toggleNavOpen={toggleNavOpen}
-      isOnline={isOnline}
-      isStaging={isStaging}
-      testid="mobile-bidlist-header"
-    />
-    <h1 data-testid="bid-job-title-mobile" className={styles.mobileTitle}>
-      {job.title}
-    </h1>
-    <BidSections
-      job={job}
-      bids={bids}
-      propertyId={propertyId}
-      colors={colors}
-      configBids={configBids}
-    />
-  </>
-);
+}) => {
+  const config = getConfig() || {};
+  const publicRuntimeConfig = config.publicRuntimeConfig || {};
+  const basePath = publicRuntimeConfig.basePath || '';
+
+  const newBidLink = `${basePath}/properties/${propertyId}/jobs/${job.id}/bids/new`;
+
+  // Mobile Header actions buttons
+  const mobileHeaderActions = (headStyle) => (
+    <>
+      <Link href={newBidLink}>
+        <a className={clsx(headStyle.header__button)}>
+          <AddIcon />
+        </a>
+      </Link>
+    </>
+  );
+  return (
+    <>
+      <MobileHeader
+        title={`Bids (${bids.length})`}
+        toggleNavOpen={toggleNavOpen}
+        isOnline={isOnline}
+        isStaging={isStaging}
+        actions={mobileHeaderActions}
+        testid="mobile-bidlist-header"
+      />
+      <h1 data-testid="bid-job-title-mobile" className={styles.mobileTitle}>
+        {job.title}
+      </h1>
+      <BidSections
+        job={job}
+        bids={bids}
+        propertyId={propertyId}
+        colors={colors}
+        configBids={configBids}
+      />
+    </>
+  );
+};
 
 export default MobileLayout;
