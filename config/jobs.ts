7677201841b60dd67aa71b5eaec73a7ea4252cb1
Matthew Jensen <mjens02@gmail.com>
2021-10-08 17:39:24 -0500

    #348 add updated project type descriptions to job edit form

--- config/jobs.ts
+++ config/jobs.ts
@@ -21,22 +21,22 @@ export default {
     'small:pm': {
       title: 'Property Management Project',
       description:
-        'PMT will collect a minimum of 2 competitive bids from national and/or local vendors. (3+ bids recommended)'
+        'Projects that are less than $5K and don’t require a contract (+2 Bids)'
     },
     'small:hybrid': {
       title: 'Hybrid Capital Project',
       description:
-        'PMT will collect a minimum of 2 competitive bids from national and/or local vendors. (3+ bids recommended)'
+        'Large capital projects that are $5K - $24K (+2 Bids)'
     },
     'large:am': {
       title: 'Asset Management Project',
       description:
-        'Collect a minimum of 3 competitive bids from national and/or local vendors'
+        'Projects costing over $25K (+3 Bids)'
     },
     'large:sc': {
       title: 'Structural Capital Project',
       description:
-        'Collect a minimum of 3 competitive bids from national and/or local vendors.'
+        'Any project that requires structural work, regardless of cost (+3 Bids)'
     }
   }
 };
