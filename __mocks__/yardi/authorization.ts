d6f223aa5154e375030beb7b54f99f71a83fde62
Matthew Jensen <mjens02@gmail.com>
2021-10-28 18:30:28 -0500

    #392 refactor property controller and add test coverage

---
+++ __mocks__/yardi/authorization.ts
@@ -0,0 +1,10 @@
+const authorization = {
+  id: 'yardi',
+  createdAt: 1586461293,
+  updatedAt: 1586461293,
+  grantedBy: 'm3sDb273',
+  yardiEntity: 'Sparkle Standard',
+  yardiUserName: 'sparkle'
+};
+
+export default authorization;
--- pages/properties/[id].tsx
+++ pages/properties/[id].tsx
@@ -5,17 +5,63 @@ import { useUser, useFirestore } from 'reactfire';
 import { MainLayout } from '../../common/MainLayout';
 import PropertyProfile from '../../features/PropertyProfile';
 import useFirestoreUser from '../../common/hooks/useFirestoreUser';
+import useProperty from '../../common/hooks/useProperty';
+import usePropertyInspections from '../../features/PropertyProfile/hooks/usePropertyInspections';
+import useYardiIntegration from '../../features/PropertyProfile/hooks/useYardiIntegration';
+import useTemplateCategories from '../../common/hooks/useTemplateCategories';
+import LoadingHud from '../../common/LoadingHud';
 
 const Page: React.FC = (): ReactElement => {
   const firestore = useFirestore();
   const { data: authUser } = useUser();
   const router = useRouter();
   const { id } = router.query;
-  const { data: user } = useFirestoreUser(firestore, authUser.uid || '');
+  const { data: user, status: userStatus } = useFirestoreUser(
+    firestore,
+    authUser.uid || ''
+  );
   const propertyId = typeof id === 'string' ? id : id[0];
+
+  // Fetch the data of property profile
+  const { data: property, status: propertyStatus } = useProperty(
+    firestore,
+    propertyId
+  );
+
+  // Fetch all data in template categories
+  const { data: templateCategories, status: templateCategoriesStatus } =
+    useTemplateCategories(firestore);
+
+  // Query property inspection records
+  const { data: inspections, status: inspectionsStatus } =
+    usePropertyInspections(firestore, propertyId);
+
+  // Query property inspection records
+  const { data: yardiAuthorizer } = useYardiIntegration(firestore);
+
+  let isLoaded = false;
+  if (
+    propertyStatus === 'success' &&
+    userStatus === 'success' &&
+    templateCategoriesStatus === 'success' &&
+    inspectionsStatus === 'success'
+  ) {
+    isLoaded = true;
+  }
   return (
     <MainLayout>
-      {user && <PropertyProfile user={user} id={propertyId} />}
+      {isLoaded ? (
+        <PropertyProfile
+          user={user}
+          id={propertyId}
+          property={property}
+          templateCategories={templateCategories}
+          inspections={inspections}
+          yardiAuthorizer={yardiAuthorizer}
+        />
+      ) : (
+        <LoadingHud />
+      )}
     </MainLayout>
   );
 };
