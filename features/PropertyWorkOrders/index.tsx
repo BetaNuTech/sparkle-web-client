d7a68eb5664b1ccf855c2bc2ad9fca627b1d0885
Matthew Jensen <mjens02@gmail.com>
2021-12-16 14:10:13 -0500

    #468 add property work order sorting and mobile header

--- features/PropertyWorkOrders/index.tsx
+++ features/PropertyWorkOrders/index.tsx
@@ -1,31 +1,67 @@
+import clsx from 'clsx';
 import { FunctionComponent } from 'react';
+import propertyModel from '../../common/models/property';
 import workOrderModel from '../../common/models/yardi/workOrder';
 import WorkOrderList from './List';
+import useSorting from './hooks/useSorting';
+import FolderIcon from '../../public/icons/ios/folder.svg';
+import MobileHeader from '../../common/MobileHeader';
 import styles from './styles.module.scss';
 
 interface Props {
   isOnline?: boolean;
   isStaging?: boolean;
   isNavOpen?: boolean;
-  toggleNavOpen?(): void;
   forceVisible?: boolean;
+  property: propertyModel;
   workOrders: workOrderModel[];
 }
 
-const PropertyWorkOrders: FunctionComponent<Props> = ({ workOrders }) => (
-  <>
-    <header className={styles.header} data-testid="workorders-header">
-      <h1 className={styles.header__title}>Work Orders</h1>
-    </header>
-    <WorkOrderList workOrders={workOrders} />
-  </>
-);
+const PropertyWorkOrders: FunctionComponent<Props> = ({
+  workOrders,
+  property,
+  isOnline,
+  isStaging
+}) => {
+  const { sortedWorkOrders, userFacingSortBy, nextWorkOrdersSort } =
+    useSorting(workOrders);
+
+  // Mobile Header actions buttons
+  const mobileHeaderActions = (headStyle) => (
+    <>
+      <button
+        className={headStyle.header__button}
+        data-testid="mobile-property-profile-sort-by"
+        onClick={nextWorkOrdersSort}
+      >
+        <FolderIcon />
+      </button>
+    </>
+  );
+
+  return (
+    <>
+      <MobileHeader
+        title={`${property.name.toUpperCase()} WOs`}
+        isOnline={isOnline}
+        isStaging={isStaging}
+        actions={mobileHeaderActions}
+      />
+      <footer
+        className={clsx(styles.header__footer)}
+        data-testid="property-workOrders-mobile-footer"
+      >
+        Sorted by {userFacingSortBy}
+      </footer>
+      <WorkOrderList workOrders={sortedWorkOrders} />
+    </>
+  );
+};
 
 PropertyWorkOrders.defaultProps = {
   isOnline: false,
   isStaging: false,
   isNavOpen: false,
-  toggleNavOpen: () => {}, // eslint-disable-line
   forceVisible: false
 };
 
