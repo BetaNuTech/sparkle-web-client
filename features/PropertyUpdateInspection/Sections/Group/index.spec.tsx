ef97a1ce71a4203e36f96f6d711468c0a9a190db
Matthew Jensen <mjens02@gmail.com>
2022-01-07 10:07:50 -0500

    #526 add required attachment animation to inspection items

--- features/PropertyUpdateInspection/Sections/Group/index.spec.tsx
+++ features/PropertyUpdateInspection/Sections/Group/index.spec.tsx
@@ -2,7 +2,16 @@ import sinon from 'sinon';
 import { act, render as rtlRender, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Context as ResponsiveContext } from 'react-responsive';
-import { originalMultiSection } from '../../../../__mocks__/inspections';
+import stubIntersectionObserver from '../../../../__tests__/helpers/stubIntersectionObserver';
+import {
+  originalMultiSection,
+  singleSection,
+  unselectedCheckmarkItem,
+  unselectedThumbsItem,
+  unselectedAbcItem,
+  unselectedOneToFiveItem,
+  unselectedCheckedExclaimItem
+} from '../../../../__mocks__/inspections';
 import breakpoints from '../../../../config/breakpoints';
 import SectionItem from './index';
 
@@ -56,5 +65,63 @@ describe('Unit | Features | Property Update Inspection | Sections | Group', () =
     ).id;
     expect(onSectionCollapseToggle.called).toBeTruthy();
     expect(sectionCollapseToggleId).toEqual(originalMultiSection.id);
+  });
+
+  it('should render notes and attachment icons with deficiency when item has deficient selection', async () => {
+    stubIntersectionObserver();
+    const onSectionCollapseToggle = sinon.spy();
+
+    const sectionItems = new Map();
+    sectionItems.set(singleSection.id, [
+      { ...unselectedCheckmarkItem, mainInputSelection: 1 },
+      { ...unselectedThumbsItem, mainInputSelection: 1 },
+      { ...unselectedAbcItem, mainInputSelection: 1 },
+      { ...unselectedOneToFiveItem, mainInputSelection: 1 },
+      { ...unselectedCheckedExclaimItem, mainInputSelection: 1 }
+    ]);
+
+    const props = {
+      forceVisible: true,
+      onInputChange: sinon.spy(),
+      onClickOneActionNotes: sinon.spy(),
+      onItemIsNAChange: sinon.spy(),
+      onClickAttachmentNotes: sinon.spy(),
+      onClickSignatureInput: sinon.spy(),
+      onClickPhotos: sinon.spy(),
+      inspectionItemsPhotos: new Map(),
+      inspectionItemsSignature: new Map(),
+      sectionItems,
+      section: singleSection,
+      nextSectionTitle: '',
+      collapsedSections: [],
+      onAddSection: sinon.spy(),
+      onRemoveSection: sinon.spy(),
+      onSectionCollapseToggle,
+      canEdit: true,
+      isMobile: false,
+      completedItems: [],
+      requireDeficientItemNoteAndPhoto: true,
+      isIncompleteRevealed: true
+    };
+
+    render(<SectionItem {...props} />, {
+      contextWidth: breakpoints.desktop.minWidth
+    });
+
+    // waiting for deficientItemList to be updated in state
+    await act(async () => {
+      await new Promise((r) => setTimeout(r, 100));
+    });
+
+    const attachmentNotes = screen.queryAllByTestId('attachment-note');
+    const attachmentPhotos = screen.queryAllByTestId('attachment-photo');
+
+    attachmentNotes.forEach((note) => {
+      expect(note.dataset.testdeficient).toEqual('deficient');
+    });
+
+    attachmentPhotos.forEach((photo) => {
+      expect(photo.dataset.testdeficient).toEqual('deficient');
+    });
   });
 });
