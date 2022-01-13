fbbf7f1fc8d37012a6a36763267acf1aa0f7bf0d
Matthew Jensen <mjens02@gmail.com>
2022-01-13 09:57:56 -0500

    #546 add deficient item edit form header

---
+++ features/DeficientItemEdit/Header/Breadcrumbs.tsx
@@ -0,0 +1,31 @@
+import { FunctionComponent } from 'react';
+import Link from 'next/link';
+import PropertyModel from '../../../common/models/property';
+
+import styles from './styles.module.scss';
+
+interface Props {
+  property: PropertyModel;
+  itemTitle: string;
+}
+
+const Breadcrumbs: FunctionComponent<Props> = ({ property, itemTitle }) => (
+  <div className={styles.header}>
+    <div className={styles.header__breadcrumb}>
+      <Link href={`/properties/${property.id}`}>
+        <a className={styles.header__propertyName}>{`${property.name}`}</a>
+      </Link>
+      <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>
+      <Link href={`/properties/${property.id}/deficient-items`}>
+        <a className={styles.header__propertyName}>Deficient Items</a>
+      </Link>
+
+      <span className={styles.header__breadcrumb}>
+        &nbsp;&nbsp;/&nbsp;&nbsp;{itemTitle}
+      </span>
+    </div>
+    <div className={styles.header__title}>{itemTitle}</div>
+  </div>
+);
+
+export default Breadcrumbs;
--- features/DeficientItemEdit/index.tsx
+++ features/DeficientItemEdit/index.tsx
@@ -1,16 +1,45 @@
 import { FunctionComponent } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import breakpoints from '../../config/breakpoints';
 import propertyModel from '../../common/models/property';
 import DeficientItemModel from '../../common/models/deficientItem';
 import userModel from '../../common/models/user';
+import Header from './Header';
 
 interface Props {
   user: userModel;
   property: propertyModel;
   deficientItem: DeficientItemModel;
+  isOnline?: boolean;
+  isStaging?: boolean;
 }
 
-const DeficientItemEdit: FunctionComponent<Props> = () => (
-  <div>Deficient Item Form Page</div>
-);
+const DeficientItemEdit: FunctionComponent<Props> = ({
+  property,
+  deficientItem,
+  isOnline,
+  isStaging
+}) => {
+  // Responsive queries
+  const isTablet = useMediaQuery({
+    maxWidth: breakpoints.tablet.maxWidth
+  });
+
+  const isDesktop = useMediaQuery({
+    minWidth: breakpoints.desktop.minWidth
+  });
+  return (
+    <>
+      <Header
+        property={property}
+        isOnline={isOnline}
+        isStaging={isStaging}
+        isTablet={isTablet}
+        isDesktop={isDesktop}
+        itemTitle={deficientItem.itemTitle}
+      />
+    </>
+  );
+};
 
 export default DeficientItemEdit;
