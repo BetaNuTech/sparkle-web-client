ae71a061d38e1f0352a6469036bcbedc6772e9f6
Matthew Jensen <mjens02@gmail.com>
2022-01-17 18:14:51 -0500

    #557 add deficient items notes form field

--- __mocks__/deficientItems.ts
+++ __mocks__/deficientItems.ts
@@ -21,3 +21,26 @@ export const deficientItem: deficientItemModal = {
   willRequireProgressNote: true,
   isDuplicate: false
 };
+
+export const deficientItemWithNotes = {
+  id: 'deficiency-1',
+  createdAt: 1620952610,
+  updatedAt: 1620952610,
+  property: 'property-1',
+  inspection: 'inspection-1',
+  item: 'item-1',
+  stateHistory: {},
+  state: 'completed',
+  sectionTitle: 'A',
+  sectionType: 'single',
+  sectionSubtitle: '',
+  itemTitle: 'Five',
+  itemMainInputType: 'FiveActions_oneToFive',
+  itemMainInputSelection: 2,
+  itemScore: 3,
+  itemAdminEdits: {},
+  hasItemPhotoData: true,
+  willRequireProgressNote: true,
+  isDuplicate: false,
+  itemInspectorNotes: 'this is inspector notes'
+};
