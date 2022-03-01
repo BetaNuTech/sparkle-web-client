a9016a17bdfece8f717525c0e4aa824566ab3263
Matthew Jensen <mjens02@gmail.com>
2022-03-01 10:47:22 -0500

    #710 fix single bulk update checkbox redirecting on firefox

--- features/DeficientItems/StateGroups/List/Item/index.tsx
+++ features/DeficientItems/StateGroups/List/Item/index.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import { FunctionComponent, useMemo, useRef } from 'react';
+import Router from 'next/router';
 import moment from 'moment';
-import LinkFeature from '../../../../../common/LinkFeature';
 import deficientItemModel from '../../../../../common/models/deficientItem';
 import getResponsibilityGroup from '../../../../../common/utils/deficientItem/getResponsibilityGroup';
 import features from '../../../../../config/features';
@@ -73,20 +73,62 @@ const DeficientItemsStateGroupsListItem: FunctionComponent<Props> = ({
     (deficientItem.currentDeferredDate && deferredDate) ||
     (deficientItem.currentDueDate ? dueDate : 'Due: Not Set');
 
+  const onClickItem = () => {
+    if (features.supportBetaDeficientItemEdit) {
+      Router.push(
+        `/properties/${deficientItem.property}/deficient-items/edit/${deficientItem.id}`
+      );
+    } else {
+      window.location.href = `/properties/${deficientItem.property}/deficient-items/${deficientItem.id}`;
+    }
+  };
+
   return (
     <li ref={placeholderRef} className={styles.container}>
       {isVisible && (
-        <LinkFeature
-          href={`/properties/${deficientItem.property}/deficient-items/edit/${deficientItem.id}`}
-          legacyHref={`/properties/${deficientItem.property}/deficient-items/${deficientItem.id}`}
-          featureEnabled={features.supportBetaDeficientItemEdit}
-        >
-          <div className={styles.main}>
-            <div
-              className={clsx(styles.row, !isMobile && styles['row--reverse'])}
-            >
-              {!isMobile && (
-                <span className={styles.icon}>
+        <div className={styles.main} onClick={onClickItem}>
+          <div
+            className={clsx(styles.row, !isMobile && styles['row--reverse'])}
+          >
+            {!isMobile && (
+              <span className={styles.icon}>
+                <SelectionIcon
+                  itemMainInputType={deficientItem.itemMainInputType}
+                  itemMainInputSelection={deficientItem.itemMainInputSelection}
+                />
+              </span>
+            )}
+            <div className={styles.row__left}>
+              <span
+                className={clsx(
+                  styles.badge,
+                  styles[`badge--${deficientItem.currentResponsibilityGroup}`],
+                  isClosed && isMobile && '-o-50'
+                )}
+                data-testid="current-responsibility-group"
+              >
+                {getResponsibilityGroup(
+                  deficientItem.currentResponsibilityGroup
+                ) || 'Not Set'}
+              </span>
+            </div>
+            <div className={styles.row__right}>
+              <span
+                className={clsx(
+                  styles.badge,
+                  styles[`badge--${deficientItem.state}`]
+                )}
+                data-testid="item-right-badge"
+              >
+                {rightBadgeText}
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            {isMobile && (
+              <div className={styles.row__left}>
+                <span className={clsx(styles.icon, isClosed && '-o-50')}>
                   <SelectionIcon
                     itemMainInputType={deficientItem.itemMainInputType}
                     itemMainInputSelection={
@@ -94,110 +136,65 @@ const DeficientItemsStateGroupsListItem: FunctionComponent<Props> = ({
                     }
                   />
                 </span>
-              )}
-              <div className={styles.row__left}>
-                <span
-                  className={clsx(
-                    styles.badge,
-                    styles[
-                      `badge--${deficientItem.currentResponsibilityGroup}`
-                    ],
-                    isClosed && isMobile && '-o-50'
-                  )}
-                  data-testid="current-responsibility-group"
-                >
-                  {getResponsibilityGroup(
-                    deficientItem.currentResponsibilityGroup
-                  ) || 'Not Set'}
-                </span>
               </div>
-              <div className={styles.row__right}>
-                <span
-                  className={clsx(
-                    styles.badge,
-                    styles[`badge--${deficientItem.state}`]
-                  )}
-                  data-testid="item-right-badge"
-                >
-                  {rightBadgeText}
-                </span>
-              </div>
-            </div>
-
-            <div className={styles.row}>
-              {isMobile && (
-                <div className={styles.row__left}>
-                  <span className={clsx(styles.icon, isClosed && '-o-50')}>
-                    <SelectionIcon
-                      itemMainInputType={deficientItem.itemMainInputType}
-                      itemMainInputSelection={
-                        deficientItem.itemMainInputSelection
-                      }
-                    />
-                  </span>
-                </div>
-              )}
-              <div className={styles.row__right}>
-                <span
-                  className={clsx(
-                    styles.title,
-                    '-mb-sm',
-                    isClosed && isMobile && '-c-gray-light'
-                  )}
-                  data-testid="item-title"
-                >
-                  {!isMobile && !isClosed && (
-                    <input
-                      type="checkbox"
-                      checked={
-                        selectedDeficiencies.indexOf(deficientItem.id) > -1
-                      }
-                      data-testid="item-checkbox"
-                      className="-mr"
-                      onChange={() =>
-                        onSelectDeficiency(
-                          deficientItem.state,
-                          deficientItem.id
-                        )
-                      }
-                      onClick={(e) => e.stopPropagation()}
-                    />
-                  )}
-                  {deficientItem.itemTitle}
-                </span>
-                <span
-                  className={clsx(
-                    styles.title,
-                    isClosed && isMobile && '-c-info',
-                    !isClosed && isMobile && '-c-primary-dark',
-                    !isMobile && styles.sectionTitle
-                  )}
-                  data-testid="section-title"
-                >
-                  {deficientItem.sectionTitle}
-                  {deficientItem.sectionSubtitle && !isMobile && (
-                    <span data-testid="section-sub-title">
-                      {' '}
-                      / {deficientItem.sectionSubtitle}
-                    </span>
-                  )}
-                </span>
-                {deficientItem.sectionSubtitle && isMobile && (
-                  <span
-                    className={clsx(
-                      styles.title,
-                      isClosed ? '-c-info' : '-c-primary-dark',
-                      !isMobile && styles.sectionTitle
-                    )}
-                    data-testid="section-sub-title"
-                  >
-                    {deficientItem.sectionSubtitle}
+            )}
+            <div className={styles.row__right}>
+              <span
+                className={clsx(
+                  styles.title,
+                  '-mb-sm',
+                  isClosed && isMobile && '-c-gray-light'
+                )}
+                data-testid="item-title"
+              >
+                {!isMobile && !isClosed && (
+                  <input
+                    type="checkbox"
+                    checked={
+                      selectedDeficiencies.indexOf(deficientItem.id) > -1
+                    }
+                    data-testid="item-checkbox"
+                    className="-mr"
+                    onChange={() =>
+                      onSelectDeficiency(deficientItem.state, deficientItem.id)
+                    }
+                    onClick={(e) => e.stopPropagation()}
+                  />
+                )}
+                {deficientItem.itemTitle}
+              </span>
+              <span
+                className={clsx(
+                  styles.title,
+                  isClosed && isMobile && '-c-info',
+                  !isClosed && isMobile && '-c-primary-dark',
+                  !isMobile && styles.sectionTitle
+                )}
+                data-testid="section-title"
+              >
+                {deficientItem.sectionTitle}
+                {deficientItem.sectionSubtitle && !isMobile && (
+                  <span data-testid="section-sub-title">
+                    {' '}
+                    / {deficientItem.sectionSubtitle}
                   </span>
                 )}
-              </div>
+              </span>
+              {deficientItem.sectionSubtitle && isMobile && (
+                <span
+                  className={clsx(
+                    styles.title,
+                    isClosed ? '-c-info' : '-c-primary-dark',
+                    !isMobile && styles.sectionTitle
+                  )}
+                  data-testid="section-sub-title"
+                >
+                  {deficientItem.sectionSubtitle}
+                </span>
+              )}
             </div>
           </div>
-        </LinkFeature>
+        </div>
       )}
     </li>
   );
