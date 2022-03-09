8eb20070340fe6460eee68e7be8c0648fee83953
Matthew Jensen <mjens02@gmail.com>
2022-03-09 12:10:30 -0500

    #738 add template category delete request

--- features/Templates/ManageCategoriesModal/item/Dropdown/index.tsx
+++ features/Templates/ManageCategoriesModal/item/Dropdown/index.tsx
@@ -3,11 +3,19 @@ import ActionsIcon from '../../../../../public/icons/ios/actions.svg';
 import Dropdown, { DropdownButton } from '../../../../../common/Dropdown';
 import styles from './styles.module.scss';
 
-const ManageCategoriesDropdown: FunctionComponent = () => (
+interface Props {
+  onDeleteCategory(): void;
+}
+
+const ManageCategoriesDropdown: FunctionComponent<Props> = ({
+  onDeleteCategory
+}) => (
   <div className={styles.container}>
     <ActionsIcon />
     <Dropdown>
-      <DropdownButton type="button">Delete</DropdownButton>
+      <DropdownButton onClick={onDeleteCategory} type="button">
+        Delete
+      </DropdownButton>
     </Dropdown>
   </div>
 );
