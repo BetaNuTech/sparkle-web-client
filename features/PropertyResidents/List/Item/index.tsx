e0bb4aaa04531ed1a8dd95101957ac5ad73f7312
Matthew Jensen <mjens02@gmail.com>
2022-03-28 10:34:40 -0500

    #758 desktop residents UI and search/sort controls

--- features/PropertyResidents/List/Item/index.tsx
+++ features/PropertyResidents/List/Item/index.tsx
@@ -8,109 +8,118 @@ import Info from './Info';
 
 interface Props {
   resident: ResidentModel;
+  isMobile: boolean;
 }
 
-const ResidenceListItem: FunctionComponent<Props> = ({ resident }) => {
-  const hasName = Boolean(
-    resident.firstName || resident.middleName || resident.lastName
-  );
-
-  return (
-    <li className={styles.container}>
-      <div className={styles.badgeContainer}>
-        {resident.id && (
-          <div className={styles.leftBadge}>
-            <span data-testid="id">{resident.id}</span>
-          </div>
-        )}
-
-        {resident.yardiStatus && (
-          <div className={styles.rightBadge}>
-            <span data-testid="resindents-yardi-status">
-              {resident.yardiStatus}
-            </span>
-          </div>
-        )}
-      </div>
-      {hasName && (
-        <div className={styles.info}>
-          <p className={styles.info__label}>Name:</p>
-          {resident.firstName && (
-            <span
-              className={styles.info__value}
-              data-testid="resindents-first-name"
-            >
-              {resident.firstName}
-            </span>
+const ResidenceListItem: FunctionComponent<Props> = ({
+  resident,
+  isMobile
+}) => (
+  <li className={styles.container}>
+    <div className={styles.container__left}>
+      {isMobile && (
+        <div className={styles.badgeContainer}>
+          {resident.id && (
+            <div className={styles.leftBadge}>
+              <span data-testid="residents-id">{resident.id}</span>
+            </div>
           )}
-          {resident.middleName && (
-            <span
-              className={styles.info__value}
-              data-testid="resindents-middle-name"
-            >
-              {resident.middleName}
-            </span>
-          )}
-          {resident.lastName && (
-            <span
-              className={styles.info__value}
-              data-testid="resindents-last-name"
-            >
-              {resident.lastName}
-            </span>
+
+          {resident.yardiStatus && (
+            <div className={styles.rightBadge}>
+              <span data-testid="residents-yardi-status">
+                {resident.yardiStatus}
+              </span>
+            </div>
           )}
         </div>
       )}
+      <div className={styles.title}>
+        {resident.firstName && (
+          <span data-testid="residents-first-name">{resident.firstName}</span>
+        )}
+        {resident.middleName && (
+          <span data-testid="residents-middle-name">{resident.middleName}</span>
+        )}
+        {resident.lastName && (
+          <span data-testid="residents-last-name">{resident.lastName}</span>
+        )}
+      </div>
+      {resident.id && (
+        <Info label="ID" value={resident.id} data-testid="residents-id" />
+      )}
+      {resident.status && (
+        <Info
+          label="Status"
+          value={resident.status}
+          data-testid="residents-yardi-status"
+        />
+      )}
+      <div className={styles.info}>
+        {resident.leaseUnit && (
+          <>
+            <p className={styles.info__label}>Unit:</p>
+            <span
+              className={styles.info__value}
+              data-testid="residents-lease-unit"
+            >
+              {resident.leaseUnit}
+            </span>
+          </>
+        )}
+        {resident.sortLeaseUnit && (
+          <>
+            <p className={styles.info__label}>Sq Ft:</p>
+            <span
+              className={styles.info__value}
+              data-testid="residents-sort-lease-unit"
+            >
+              {resident.sortLeaseUnit}
+            </span>
+          </>
+        )}
+      </div>
 
-      {resident.leaseUnit && (
-        <Info
-          label="Unit"
-          value={resident.leaseUnit}
-          data-testid="resindents-lease-unit"
-        />
+      {isMobile && (
+        <>
+          {resident.email && (
+            <Info
+              label="Email"
+              value={resident.email}
+              data-testid="residents-email"
+            />
+          )}
+          {resident.mobileNumber && (
+            <Info
+              label="Mobile Number"
+              value={phoneNumber(resident.mobileNumber)}
+              data-testid="residents-mobile-number"
+            />
+          )}
+          {resident.officeNumber && (
+            <Info
+              label="Office Number"
+              value={phoneNumber(resident.officeNumber)}
+              data-testid="residents-office-number"
+            />
+          )}
+          {resident.homeNumber && (
+            <Info
+              label="Home Number"
+              value={phoneNumber(resident.homeNumber)}
+              data-testid="residents-home-number"
+            />
+          )}
+        </>
       )}
-      {resident.sortLeaseUnit && (
-        <Info
-          label="Sq Ft"
-          value={resident.sortLeaseUnit}
-          data-testid="resindents-sort-lease-unit"
-        />
-      )}
-      {resident.email && (
-        <Info
-          label="Email"
-          value={resident.email}
-          data-testid="resindents-email"
-        />
-      )}
-      {resident.mobileNumber && (
-        <Info
-          label="Mobile Number"
-          value={phoneNumber(resident.mobileNumber)}
-          data-testid="resindents-mobile-number"
-        />
-      )}
-      {resident.officeNumber && (
-        <Info
-          label="Office Number"
-          value={phoneNumber(resident.officeNumber)}
-          data-testid="resindents-office-number"
-        />
-      )}
-      {resident.homeNumber && (
-        <Info
-          label="Home Number"
-          value={phoneNumber(resident.homeNumber)}
-          data-testid="resindents-home-number"
-        />
-      )}
+
       {resident.leaseFrom && (
         <Info
           label="Lease From Date"
           value={dateUtil.toUserFullDateDisplay(
             dateUtil.isoToTimestamp(resident.leaseFrom)
           )}
-          data-testid="resindents-lease-from"
+          data-testid="residents-lease-from"
         />
       )}
 
@@ -120,7 +129,7 @@ const ResidenceListItem: FunctionComponent<Props> = ({ resident }) => {
           value={dateUtil.toUserFullDateDisplay(
             dateUtil.isoToTimestamp(resident.leaseTo)
           )}
-          data-testid="resindents-lease-to"
+          data-testid="residents-lease-to"
         />
       )}
 
@@ -130,53 +139,85 @@ const ResidenceListItem: FunctionComponent<Props> = ({ resident }) => {
           value={dateUtil.toUserFullDateDisplay(
             dateUtil.isoToTimestamp(resident.moveIn)
           )}
-          data-testid="resindents-move-in"
+          data-testid="residents-move-in"
         />
       )}
 
       {resident.occupants.length > 0 && (
         <div className="-mt -mb">
-          <div className={styles.info}>
-            <p className={styles.info__label}>Occupants:</p>
+          <div className={styles.row}>
+            <p className={styles.info__label}>Roommate:</p>
           </div>
           {resident.occupants.map((occupant) => (
             <OccupantItem occupant={occupant} key={occupant.id} />
           ))}
         </div>
       )}
-
       {typeof resident.totalCharges === 'number' && (
         <Info label="Total Charges" value={moneyUsd(resident.totalCharges)} />
       )}
-
       {typeof resident.totalOwed === 'number' && (
         <Info label="Total Owed" value={moneyUsd(resident.totalOwed)} />
       )}
-
       {typeof resident.paymentPlan === 'boolean' && (
         <Info
           label="Payment Plan"
           value={resident.paymentPlan ? 'YES' : 'NO'}
-          data-testid="resindents-payment-plan"
+          data-testid="residents-payment-plan"
         />
       )}
-
       {typeof resident.paymentPlanDelinquent === 'boolean' && (
         <Info
           label="Payment Plan Delinquent"
           value={resident.paymentPlanDelinquent ? 'YES' : 'NO'}
         />
       )}
-
       {typeof resident.eviction === 'boolean' && (
         <Info label="Eviction" value={resident.eviction ? 'YES' : 'NO'} />
       )}
-
-      {typeof resident.lastNote && (
+      {resident.lastNote && (
         <Info label="Last Note" value={resident.lastNote} />
       )}
-    </li>
-  );
-};
+    </div>
+
+    {!isMobile && (
+      <div className={styles.right}>
+        <div className={styles.badge}>
+          <ul>
+            {resident.email && (
+              <Info
+                label="Email"
+                value={resident.email}
+                data-testid="residents-email"
+              />
+            )}
+
+            {resident.mobileNumber && (
+              <Info
+                label="Mobile Number"
+                value={phoneNumber(resident.mobileNumber)}
+                data-testid="residents-mobile-number"
+              />
+            )}
+            {resident.officeNumber && (
+              <Info
+                label="Office Number"
+                value={phoneNumber(resident.officeNumber)}
+                data-testid="residents-office-number"
+              />
+            )}
+            {resident.homeNumber && (
+              <Info
+                label="Home Number"
+                value={phoneNumber(resident.homeNumber)}
+                data-testid="residents-home-number"
+              />
+            )}
+          </ul>
+        </div>
+      </div>
+    )}
+  </li>
+);
 
 export default ResidenceListItem;
--- features/PropertyResidents/List/index.tsx
+++ features/PropertyResidents/List/index.tsx
@@ -1,17 +1,22 @@
 import { FunctionComponent } from 'react';
 import residentModel from '../../../common/models/yardi/resident';
 import Item from './Item';
+import styles from './styles.module.scss';
 
 interface Props {
   residents: residentModel[];
+  isMobile:boolean;
 }
 
-const ResidentList: FunctionComponent<Props> = ({ residents }) => (
-  <ul>
-    {residents.map((resident) => (
-      <Item key={resident.id} resident={resident} />
-    ))}
-  </ul>
+const ResidentList: FunctionComponent<Props> = ({ residents,isMobile }) => (
+  <div>
+    <h3 className={styles.header}>Residents</h3>
+    <ul>
+      {residents.map((resident) => (
+        <Item key={resident.id} resident={resident} isMobile={isMobile} />
+      ))}
+    </ul>
+  </div>
 );
 
 export default ResidentList;
