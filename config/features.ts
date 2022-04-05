a9247032eedf603a3647982d87c86f2cda41400e
Matthew Jensen <mjens02@gmail.com>
2022-04-05 12:32:49 -0500

    #837 work order product release and fixes

--- config/features.ts
+++ config/features.ts
@@ -17,7 +17,8 @@ export default {
     process.env.NEXT_PUBLIC_SUPPORT_BETA_PROPERTY_DEFICIENT === 'true' || false,
   supportBetaPropertyYardiResident:
     process.env.NEXT_PUBLIC_SUPPORT_BETA_YARDI_RESIDENT === 'true' || false,
-  supportBetaPropertyYardiWorkOrder: false,
+  supportBetaPropertyYardiWorkOrder:
+    process.env.NEXT_PUBLIC_SUPPORT_BETA_YARDI_WORK_ORDER === 'true' || false,
   supportBetaPropertyInspectionCreate: true,
   supportBetaPropertyInspectionUpdate: true,
   supportBetaPropertyInspectionMove: false,
