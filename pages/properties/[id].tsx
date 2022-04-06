1f9ac682d7140c1e11df88099f636cb41c51815c
Matthew Jensen <mjens02@gmail.com>
2022-04-06 10:33:34 -0500

    #839 redirect requests for 404 properties in property edit and profile

--- pages/properties/[id].tsx
+++ pages/properties/[id].tsx
@@ -1,10 +1,12 @@
 import 'firebase/firestore';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useUser, useFirestore } from 'reactfire';
 import { MainLayout } from '../../common/MainLayout';
 import PropertyProfile from '../../features/PropertyProfile';
 import useFirestoreUser from '../../common/hooks/useFirestoreUser';
+import useNotifications from '../../common/hooks/useNotifications'; // eslint-disable-line
+import notifications from '../../common/services/notifications'; // eslint-disable-line
 import useProperty from '../../common/hooks/useProperty';
 import usePropertyInspections from '../../features/PropertyProfile/hooks/usePropertyInspections';
 import useYardiIntegration from '../../features/PropertyProfile/hooks/useYardiIntegration';
@@ -15,6 +17,10 @@ const Page: React.FC = (): ReactElement => {
   const firestore = useFirestore();
   const { data: authUser } = useUser();
   const router = useRouter();
+
+  /* eslint-disable-next-line */
+  const sendNotification = notifications.createPublisher(useNotifications());
+
   const { id } = router.query;
   const { data: user, status: userStatus } = useFirestoreUser(
     firestore,
@@ -38,6 +44,19 @@ const Page: React.FC = (): ReactElement => {
 
   // Query property inspection records
   const { data: yardiAuthorizer } = useYardiIntegration(firestore);
+
+  // using this useEffect so it only
+  // sends single notification to user
+  useEffect(() => {
+    // Redirect to properties if property not found with error notification
+    if (!property?.name && propertyStatus === 'success') {
+      sendNotification('Property does not exist.', {
+        type: 'error'
+      });
+      router.push('/properties/');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [propertyStatus, property]);
 
   let isLoaded = false;
   if (
